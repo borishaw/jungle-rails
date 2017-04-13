eb91ab71b0a3268d7620be2915a0adec70ccfd59
Boris Xiao <borishaw@gmail.com>
2017-04-13 20:15:20 +0000

    added users feature

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,17 @@ Rails.application.routes.draw do
     resources :categories
   end
 
+
+  # Routes to handle user registration
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  # Routes to handle user login
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
